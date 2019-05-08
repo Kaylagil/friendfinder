bf07ffa17cb4bc81d506cbd4ee68904aa5f35948
Kaylagil <44254198+Kaylagil@users.noreply.github.com>
2019-05-07 21:29:25 -0400

    final

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Kayla",
+		photo: "/app/data/images.jpeg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Bob",
+		photo: "/app/data/images.jpeg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Bill",
+		photo: "/app/data/images.jpeg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Bill 2",
+		photo: "/app/data/images.jpeg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "sam ",
+		photo: "/app/data/images.jpeg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
